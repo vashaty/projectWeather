b65fc05e45c359f440c65817ec849e81d963a67d
vashaty <v_toncer@utb.cz>
2020-08-10 12:41:31 +0200

    optimalizace, poznámky

--- js/app.js
+++ js/app.js
@@ -1,48 +1,19 @@
 var input = document.querySelector('.input_text');
-var main = document.querySelector('#name');
-// var temp = document.querySelector('#t0');
-// var date = document.querySelector('#da0');
-// var desc = document.querySelector('#de0');
-// var clouds = document.querySelector('#c0');
 var button= document.querySelector('.submit');
-
-
-// button.addEventListener('click', function(name){
-//   //http://api.openweathermap.org/data/2.5/forecast?q=London&appid=efa340fdb65947b231e403f7a0a9c21a
-// fetch('http://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&appid=efa340fdb65947b231e403f7a0a9c21a&units=metric&lang=cz')
-// .then(response => response.json())
-// .then(data => {
-//   var dateValue = data['main'][1]['temp'];
-//   var tempValue = data['main'][1]['dt_txt'];
-//   var nameValue = data['city']['name'];
-//   var descValue = data['weather'][1]['description'];
-
-//   main.innerHTML = nameValue;
-//   desc.innerHTML = "Desc - "+descValue;
-//   temp.innerHTML = "Temp - "+tempValue;
-//   temp.innerHTML = "Date - "+dateValue;
-//   input.value ="";
-
-// })
-
-// .catch(err => alert("Wrong city name!"));
-// })
 var input = document.getElementById("myInput");
 
-// Execute a function when the user releases a key on the keyboard
+//přitání funkce enteru
 input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
-    // Cancel the default action, if needed
     event.preventDefault();
-    // Trigger the button element with a click
     document.getElementById("myBtn").click();
   }
 });
 
-var xd = 0;
+var xd = 0; //proměnná pro zjištění aktivních tabulek
 function forecast(name) 
 {
+  //smazání existujících tabulek
     if(xd != 0){
       document.getElementById("nameDiv").remove();
         document.getElementById("favDiv");
@@ -65,46 +36,58 @@ function forecast(name)
 
     .then(function (myJson) 
     {
-        // var today = new Date();
-        // var time = today.getDate()+'. '+(today.getMonth()+1) +". "+ today.getFullYear() + " " +today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();  
-        // resultInput.value = myJson.responseData.translatedText;
+
         console.log(myJson);
-        // loadingDiv.style.display = 'none';
-        // localStorage.setItem(localStorage.length, time +" -- "+userInput.value + " -> " + resultInput.value);
-        // makeHistory();
-        // localStorage.clear();
+
+        //vytvoření divu na jméno a tlačítko pro přidání oblíbených
         var nameDiv = document.createElement("div");
         nameDiv.setAttribute("id","nameDiv");
+
         var favBtn = document.createElement("input");
         var hName = document.createElement("h1");
         hName.setAttribute("class","name");
         hName.setAttribute("id","name");
         favBtn.setAttribute("type","submit");
+
+        //přepínač na tlačítko
         if(localStorage.getItem(myJson.city.name) != null){
           favBtn.setAttribute("value","Odebrat z oblíbených");
         }else{
           favBtn.setAttribute("value","Přidat do oblíbených");
         }
+
         favBtn.setAttribute("id","favBtn");
+
         hName.innerHTML = myJson.city.name;
+
+        //přidání nadpisu a tlačítka do divu
         nameDiv.appendChild(hName);
         nameDiv.appendChild(favBtn);
+
         document.getElementById("container").appendChild(nameDiv);
-        var months =["xxx","ledna","února","března","dubna","května","června","července","srpna","září","října","listopadu","prosince"];
-        var temp = "xd";
-        var weekday = ["neděle", "pondělí", "úterý","středa","čtvrtek","pátek","sobota"];
-        var j = 0;
+
+        var months =["xxx","ledna","února","března","dubna","května","června","července","srpna","září","října","listopadu","prosince"]; //pole měsícu pro překlad z číselné hodnoty na slova
+
+        var temp = "xd"; //temp proměnná pro datum
+
+        var weekday = ["neděle", "pondělí", "úterý","středa","čtvrtek","pátek","sobota"]; //pole pro překlad dnů v týdnu
+        var j = 0; // proměnná pro číslovnání tabulek
         for (i = 0; i < 40; i++) {
+            //vytvoření elementů pro nadpis (datum) a vytvoření tabluky
             var p = document.createElement("p");
             var thTime = document.createElement("th");
             var thTemperature = document.createElement("th");
             var thDesc = document.createElement("th");
             var thWind = document.createElement("th");
-            var datum = myJson.list[i].dt_txt.split(" ");
+
+            //manipulace s datem
+            var datum = myJson.list[i].dt_txt.split(" "); //rozdělení na datum a čas
+            //získání dčíselné hodnoty dnu v týdnu
             var date = new Date(datum[0]);
             var day = date.getDay();
             
-            if(temp != datum[0]){
+            //vytváření a naplnění tabulky
+            if(temp != datum[0]){ //podmínka pro zjištění, jestli se má vytvořit nová tabulka s novým datem
                 var div = document.createElement("div");
                 div.setAttribute("id", "card"+j);
                 j++;
@@ -114,16 +97,16 @@ function forecast(name)
 
                 document.getElementById("container").appendChild(div);
 
-                temp = datum[0];
-                var tempSplit = datum[0].split("-");
-                p.innerHTML = weekday[day] + " " + parseInt(tempSplit[2])+". "+months[parseInt(tempSplit[1])]+" "+tempSplit[0];
+                temp = datum[0]; //uložení aktuálního data do temp proměnné
+                var tempSplit = datum[0].split("-"); //rozdělení data na den, měsíc a rok
+                p.innerHTML = weekday[day] + " " + parseInt(tempSplit[2])+". "+months[parseInt(tempSplit[1])]+" "+tempSplit[0]; //nastavení nadpisu tabulky (převod dnu v týdnu na text, měsíce na text)
                 div.appendChild(p);
                 // var hr = document.createElement("hr");
                 // div.appendChild(hr);
-                xd++;
+                xd++; //proměnná pro zjištění, jestli byla vytvořen div s tabulkou
 
                 div.appendChild(table);
-
+                //nadpisy sloupců tabulky
                 var tr = document.createElement("tr");
                 thTime.innerHTML = "Čas";
                 tr.appendChild(thTime);
@@ -138,66 +121,43 @@ function forecast(name)
                 // var divV = document.querySelector('#card'+i);
                 
             }
-
+            //nplnění tabulky
             tr = document.createElement("tr");
             var tdTime = document.createElement("td");
             var tdTemperature = document.createElement("td");
             var tdDesc = document.createElement("td");
             var tdWind = document.createElement("td");
             var icon = document.createElement("img");
-            icon.src = "http://openweathermap.org/img/wn/"+ myJson.list[i].weather[0].icon +"@2x.png";
+            icon.src = "http://openweathermap.org/img/wn/"+ myJson.list[i].weather[0].icon +"@2x.png"; //přidání ikony
             icon.height = 30;
             icon.width = 30;
 
-            var windArrow = document.createElement("i");
+            var windArrow = document.createElement("i"); //směrová šipka větru
             windArrow.setAttribute("class", "arrow");
             windArrow.style.transform = "rotate("+myJson.list[i].wind.deg+"deg)";
 
             tdTime.innerHTML = datum[1];
             tr.appendChild(tdTime);
+
             tdTemperature.innerHTML = myJson.list[i].main.temp + "°C";
             tr.appendChild(tdTemperature);
+
             tdWind.innerHTML = myJson.list[i].wind.speed + " m/s ";
             tdWind.appendChild(windArrow);
             tr.appendChild(tdWind);
+
             tdDesc.innerHTML = myJson.list[i].weather[0].description;
             tdDesc.appendChild(icon);
             tr.appendChild(tdDesc);
             table.appendChild(tr);
+
             favBtn.onclick = function (){
               saveFav();
             }
             console.log(localStorage);
-            // p = document.createElement("p");
-            // p.innerHTML = datum[1];
-            // div.appendChild(p);
-
-            // p = document.createElement("p");
-            // p.innerHTML = myJson.list[i].main.temp + "°C";
-            // div.appendChild(p); 
-
-            // p = document.createElement("p");
-            // p.innerHTML = myJson.list[i].weather[0].description;
-            // div.appendChild(p);
-            
-            // hr = document.createElement("hr");
-            // div.appendChild(hr);
-
-
-
-
-            
-
+   
         }
-        // var dateValue = myJson.list[0].dt_txt;
-        // var tempValue = myJson.list[0].main.temp;
-        // var nameValue = myJson.city.name;
-        // var descValue = myJson.list[0].weather[0].description;
 
-        // main.innerHTML = nameValue;
-        // desc.innerHTML = descValue;
-        // temp.innerHTML = "Teplota - "+tempValue;
-        // date.innerHTML = "Datum - "+dateValue;
     }).catch(function(err) {
         alert(err);
       });
@@ -205,41 +165,46 @@ function forecast(name)
     // localStorage.setItem(userInput.value, resultInput.value);
     
 }
+
 button.onclick = function (){
   forecast(input.value);
 }
 
-var favH3 = document.getElementById("favH3");
 
+var favH3 = document.getElementById("favH3");
+//ukládání do localStorage
 function saveFav(){
-  localStorage.setItem(document.getElementById("name").innerHTML,document.getElementById("name").innerHTML);
-  if(localStorage.length == 1){
+  var hName = document.getElementById("name");
+  localStorage.setItem(hName.innerHTML,hName.innerHTML); //podle názvu města se uloží
+  if(localStorage.length == 1){ //přepínač nadpisu
     favH3.innerHTML = "Oblíbené lokality:";
   }
-  if(document.getElementById(document.getElementById("name").innerHTML) == null){
+  //přepínač popisu tlačítka add
+  if(document.getElementById(hName.innerHTML) == null){
     
       var favCity = document.createElement("input");
       favCity.setAttribute("type","submit");
       favCity.setAttribute("class","favCity");
       favCity.setAttribute("onclick","forecastFav(this)");
       // console.log(localStorage.getItem(localStorage.key(i)));
-      favCity.setAttribute("value",localStorage.getItem(document.getElementById("name").innerHTML));
-      favCity.setAttribute("id",localStorage.getItem(document.getElementById("name").innerHTML));
+      favCity.setAttribute("value",localStorage.getItem(hName.innerHTML));
+      favCity.setAttribute("id",localStorage.getItem(hName.innerHTML));
+
       document.getElementById("favDiv").appendChild(favCity);
       console.log(localStorage);
       document.getElementById("favBtn").setAttribute("value", "Odebrat z oblíbených");
     
   }else{
-    localStorage.removeItem(document.getElementById("name").innerHTML);
-    document.getElementById(document.getElementById("name").innerHTML).remove();
+    localStorage.removeItem(hName.innerHTML);
+    document.getElementById(hName.innerHTML).remove();
     document.getElementById("favBtn").setAttribute("value", "Přidat do oblíbených");
-    if(localStorage.length == 0){
+    if(localStorage.length == 0){ //přepínač nadpisu
       favH3.innerHTML = "Nebyly nalezeny žádné oblíbené lokality";
     }
   }
 }
 
-function loadFav() {
+function loadFav() {//načítání oblíbených a vytvoření tlačítek
   if(localStorage.length == 0){
     favH3.innerHTML = "Nebyly nalezeny žádné oblíbené lokality";
   }else{
@@ -260,5 +225,4 @@ function loadFav() {
 
 function forecastFav(city){
   forecast(city.value);
-  xd++;
 }
